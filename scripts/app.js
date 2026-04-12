abba0f0608474cf6aaa4defe012c1e02916e19d1
Haris <confusedgun@gmail.com>
2026-04-12 17:54:12 +0200

    dodan app.js sa fetch pozivom prema API-ju

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-
+fetch('/api/projekcije')
+    .then(res => res.json())
+    .then(podaci => {
+        if (!validiraj(podaci)) {
+            document.getElementById('sala').innerHTML = '<p class="greska">Podaci nisu validni!</p>';
+        } else {
+            prikaziSalu(podaci, 0);
+        }
+    });
