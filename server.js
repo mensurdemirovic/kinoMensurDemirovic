fc563e9925d79d35a059670fd3bd4863d5574a1a
Haris <confusedgun@gmail.com>
2026-04-12 17:50:57 +0200

    dodan node.js kako bih mogao realizirati prikaz porjekcija, rezervisanje sjedišta i sl. Umjesto baze podataka, kroistim se json fajlom

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const PUTANJA_JSON = path.join(__dirname, 'data', 'projekcije.json');
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get('/api/projekcije', (req, res) => {
+    const podaci = fs.readFileSync(PUTANJA_JSON, 'utf-8');
+    res.json(JSON.parse(podaci));
+});
+
+app.post('/api/projekcije', (req, res) => {
+    fs.writeFileSync(PUTANJA_JSON, JSON.stringify(req.body, null, 4), 'utf-8');
+    res.json({ ok: true });
+});
+
+app.listen(PORT, () => {
+    console.log('Server pokrenut na http://localhost:' + PORT);
+});
